3cfd09091c783c283dec8128d817feacb1d9d6e1
Onur <onurnis69@gmail.com>
2020-11-26 01:10:44 +0300

    JSAlert problemi Handle edildi

--- src/test/java/com/team06/utilities/Driver.java
+++ src/test/java/com/team06/utilities/Driver.java
@@ -54,7 +54,7 @@ public class Driver {
             }
         }
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
         return driver;
     }
     public static void closeDriver(){
